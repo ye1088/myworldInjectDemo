ecc20bcd530197e1a78863ca2695c1e5fcb5fd85
your lord <18631616220@163.com>
2017-08-01 08:31:30 +0800

    first

---
+++ app/src/main/java/hlx/launch/game/d.java
@@ -0,0 +1,15 @@
+package hlx.launch.game;
+
+import android.widget.Toast;
+
+import com.google.test.MainActivity;
+
+/**
+ * Created by Administrator on 2017/8/1 0001.
+ */
+
+public class d {
+    public static void bR(MainActivity mainActivity) {
+        Toast.makeText(mainActivity, "I was clicked", Toast.LENGTH_SHORT).show();
+    }
+}
