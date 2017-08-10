47d59357d560bf62d67e3882620096063199f654
your lord <18631616220@163.com>
2017-08-10 08:00:13 +0800

    添加 从 assets中拷贝数据到指定路径的方法

--- app/src/main/java/com/google/myworldinjectcode/MainActivity.java
+++ app/src/main/java/com/google/myworldinjectcode/MainActivity.java
@@ -3,6 +3,7 @@ package com.google.myworldinjectcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.utils.SUtils;
 import com.google.utils.Utils2;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Utils2.onCreate(this);
+//        Utils2.onCreate(this);
+        if(SUtils.isFirstRun(this)||!SUtils.isExistFile("/sdcard/huluxia/mctool/MC_0.14.1_huluxia.apk")){
+            SUtils.copy_data(this);
+        }
+
 
     }
 }
